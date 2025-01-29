24d8998c59dea742b6d4b8a4c22932a32d043b4e
Prince Mutungi <princeherman08@gmail.com>
2025-01-29 12:40:22 +0300

    Added service worker and manifest.json

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/assets/src/resources/assets/images/static/icn_oky.81ee4fa9e2e79c5c2e559047e17cfd90.png",
+ 
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cached Resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
